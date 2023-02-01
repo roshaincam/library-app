1623ba6e97d24af182a6fe2f11b116f43ff7c331
roshaincampbell <RoshainCampbell@yahoo.com>
2023-02-01 16:21:33 -0500

    Initial commit

---
+++ backend/spring-boot-library/src/main/java/com/core/springbootlibrary/dao/BookRepository.java
@@ -0,0 +1,14 @@
+package com.core.springbootlibrary.dao;
+
+import com.core.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+}
---
+++ frontend/react-library/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
+import {Footer} from "./layouts/NavbarAndFooter/Footer";
+import {HomePage} from "./layouts/Homepage/HomePage";
+import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
+
+export const App = () => {
+    return (
+        <div>
+            <Navbar/>
+            {/*<HomePage/>*/}
+            <SearchBooksPage/>
+            <Footer/>
+        </div>
+    );
+}
+
